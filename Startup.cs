52d6cec57d2163ecb7317a466b2d228801dcbfa8
Ryan Ottney <rottney123@gmail.com>
2020-07-06 22:41:45 -0700

    added email auth

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,8 @@ using Rekordpool.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using WebPWrecover.Services;
 
 namespace Rekordpool
 {
@@ -32,6 +34,10 @@ namespace Rekordpool
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.Configure<AuthMessageSenderOptions>(Configuration);
+            
             services.AddRazorPages();
 
             services.Configure<IdentityOptions>(options =>
