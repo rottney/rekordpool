80dc99925d18fe2345187482aac7fa044251f9a2
Ryan Ottney <rottney123@gmail.com>
2020-08-15 15:37:48 -0700

    made Track properties required

--- Models/Track.cs
+++ Models/Track.cs
@@ -6,11 +6,18 @@ namespace Rekordpool.Models
     public class Track
     {
         public int ID { get; set; }
+
+        [Required]
         [Display(Name = "Artist Name")]
         public string ArtistName { get; set; }
+
+        [Required]
         public string Title { get; set; }
+        
         [Display(Name = "Added By")]
         public string AddedBy { get; set; }
+
+        [Required]
         public string Link { get; set; }
     }
 }
